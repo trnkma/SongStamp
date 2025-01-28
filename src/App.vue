6b11ecfa41af97f8cbed66cfcdc54f2ec1eaf50a
trnkma <matthias.trnka@gmail.com>
2025-01-29 00:59:24 +0100

    AlbumDetailComp now used for result; styling changes; removed any; removed unused code;

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import LoginComponent from './components/LoginComponent.vue';
 import PlayerComponent from './components/PlayerComponent.vue';
 import TimelineComponent from './components/TimelineComponent.vue';
 import { AuthService } from './services/AuthService';
-import AlbumComponent from './components/AlbumComponent.vue';
-import AlbumDetailComponent from './components/AlbumDetailComponent.vue';
+// import AlbumComponent from './components/AlbumComponent.vue';
 import SettingsComponent from './components/SettingsComponent.vue';
 import { GameLogicService } from './services/GameLogicService';
+import AlbumDetailComponent from './components/AlbumDetailComponent.vue';
 const authService = AuthService();
 const gameLogicService = GameLogicService.getInstance();
 
@@ -24,11 +24,7 @@ if (localStorage.getItem('refresh_token')) {
   }
 }
 
-
 const settingsOpen = ref<boolean>(true);
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const currentAlbum = ref<any | null>(null);
-
 
 </script>
 
@@ -40,25 +36,19 @@ const currentAlbum = ref<any | null>(null);
     <!-- Logged-in state -->
     <template v-else-if="authService.state.isLoggedIn">
       <SettingsComponent class="settings-comp" v-if="settingsOpen" @done="settingsOpen = false"></SettingsComponent>
-      <!-- <SearchComponent class="search-comp" /> -->
-      <PlayerComponent class="player-comp" />
-      <AlbumComponent class="album-comp" v-if="currentAlbum" :album="currentAlbum"></AlbumComponent>
 
-      <!-- <button @click="showAlbum()">getDate</button> -->
+      <PlayerComponent class="player-comp" />
+
       <TimelineComponent class="timeline-comp" />
       <div class="result" v-if="gameLogicService.showResult.value && gameLogicService.currentlyPlayingTrack">
-        <AlbumComponent :album="gameLogicService.currentlyPlayingTrack.value?.album"
+        <!-- <AlbumComponent :album="gameLogicService.currentlyPlayingTrack.value?.album"
           :song-name="gameLogicService.currentlyPlayingTrack.value?.name"
-          :artist="gameLogicService.currentlyPlayingTrack.value?.artists[0].name"></AlbumComponent>
-        <!-- <img :src="gameLogicService.currentlyPlayingTrack.value?.album.images[0].url" alt="album-image"> -->
+          :artist="gameLogicService.currentlyPlayingTrack.value?.artists[0].name"></AlbumComponent> -->
+        <AlbumDetailComponent :song-details="gameLogicService.currentSongDetails.value"></AlbumDetailComponent>
         <div class="controls">
           <button @click="gameLogicService.nextRound()">Next Track</button>
         </div>
       </div>
-      <AlbumDetailComponent :songDetails="gameLogicService.songDetails.value" class="album-detail"
-        v-if="gameLogicService.displaySongDetails.value">
-      </AlbumDetailComponent>
-      <!-- <app-snackbar /> -->
     </template>
 
     <!-- Logged-out state -->
@@ -78,14 +68,17 @@ const currentAlbum = ref<any | null>(null);
   max-height: 100%;
 
   .result {
-    padding: 2rem;
     border-radius: 2rem;
     background-color: var(--col-bg);
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 50rem;
+    width: 90%;
+
+    @media(min-width: 1024px) {
+      width: 30%;
+    }
 
     .controls {
       width: 100%;
@@ -124,42 +117,19 @@ const currentAlbum = ref<any | null>(null);
   //   left: 0;
   // }
 
-  .search-comp {
-    background-color: var(--col-bg);
-    z-index: 1000;
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translate(-50%);
-    width: 100%;
-    height: auto;
-
-
-    @media(min-width: 1024px) {
-      width: 60%;
-    }
-  }
-
   .settings-comp {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     z-index: 1000;
-    width: 60vw;
-    height: fit-content;
-    max-height: 65vh;
+    width: 90vw;
+    height: 90dvh;
+    max-height: 90vh;
     background-color: var(--col-bg);
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     border-radius: 2rem;
     // overflow-y: auto;
-
-    &::after {
-      content: '';
-      width: 2rem;
-      height: 2rem;
-      background-color: red;
-    }
   }
 
   .player-comp {
@@ -188,8 +158,7 @@ const currentAlbum = ref<any | null>(null);
   .timeline-comp {
     position: absolute;
     bottom: 5%;
-    left: 50%;
-    transform: translate(-50%);
+    // transform: translate(-50%);
     width: 90%;
     // @media(min-width: 1024px) {
     //   width: 60%;
