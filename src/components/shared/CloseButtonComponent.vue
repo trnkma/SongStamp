440cff9c0016e0d461a58aa061974058337fb271
trnkma <matthias.trnka@gmail.com>
2025-01-28 22:48:44 +0100

    added closeButtonComp

---
+++ src/components/shared/CloseButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <button>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+            <path
+                d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+        </svg>
+    </button>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss" scoped>
+button {
+    background-color: transparent;
+    width: fit-content;
+    border: none;
+}
+</style>
