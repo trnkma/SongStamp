1bc59d5bd2678c5ed44ab9da231aeb63c53722a1
trnkma <matthias.trnka@gmail.com>
2025-01-28 19:55:02 +0100

    -added loadingComp (displays if playlists get searched); -added option to load more tracks; -wrapped clear input button into button to support keyboard; -changed styling;

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,25 +1,39 @@
 <template>
     <div class="search-wrapper" v-click-outside="handleClickOutside" @click="showList = true">
+        <div class="close-wrapper">
+            <button @click="emit('close')">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#e8eaed">
+                    <path
+                        d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                </svg>
+            </button>
+        </div>
         <div class="input-wrapper">
-            <input ref="searchInput" type="text" v-model="value"
+            <input ref="searchInput" type="text" v-model="inputValue" placeholder="Search playlists..."
                 @input="onInputChange(($event.target as HTMLInputElement).value)" />
-            <svg v-if="value" @click="value = ''" xmlns="http://www.w3.org/2000/svg" height="24px"
-                viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-                <path
-                    d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-            </svg>
+            <button v-if="inputValue" @click="clearInput()">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#e8eaed">
+                    <path
+                        d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                </svg>
+            </button>
         </div>
 
-
-
-        <!-- <app-loading v-if="isLoading" /> -->
+        <LoadingComponent class="loading-comp" v-if="trackService.isLoading.value" />
 
         <div class="list-wrapper" :class="{ getHeight: trackService.list.value.length !== 0 }">
-            <button v-for="(item, index) in trackService.list.value" :key="index" @click="onItemClick(item)">
+            <button class="list-item" v-for="(item, index) in trackService.list.value" :key="index"
+                @click="onItemClick(item)">
                 <SearchListItemComponent :image-source="item.images[0]?.url" :name="item.name">
                 </SearchListItemComponent>
                 <!-- <div class="tooltip">{{ item.fullName }}</div> -->
             </button>
+            <div v-if="inputValue && trackService.list.value.length > 0 && !trackService.isLoading.value"
+                class="more-items-wrapper">
+                <button @click="loadMore()">Load more</button>
+            </div>
         </div>
     </div>
 </template>
@@ -30,9 +44,10 @@ import { ref, defineEmits } from 'vue';
 import SearchListItemComponent from './SearchListItemComponent.vue';
 import vClickOutside from '../directives/ClickOutsideDirective';
 import type { MyPlaylist } from '@/types/SpotifyWebAPI';
+import LoadingComponent from './LoadingComponent.vue';
 
 
-const value = ref('');
+const inputValue = ref('');
 const showList = ref(false);
 const trackService = TrackService.getInstance();
 
@@ -52,6 +67,15 @@ const onItemClick = (playlist: MyPlaylist) => {
     emit('close');
 };
 
+const loadMore = () => {
+    trackService.getPlaylists(inputValue.value);
+}
+
+const clearInput = () => {
+    inputValue.value = '';
+    trackService.resetPlaylists();
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -64,10 +88,29 @@ const onItemClick = (playlist: MyPlaylist) => {
     height: 100%;
     width: 100%;
 
-
-    button {
-        padding: 0 1.5rem 1.5rem 0;
+    .loading-comp {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: rgba(0, 0, 0, 0.514);
     }
+
+    .close-wrapper {
+        display: flex;
+        align-items: center;
+
+        button {
+            background-color: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+            padding: 0;
+            margin: 1rem 1rem 1rem auto;
+        }
+    }
+
+    button {}
 
     .input-wrapper {
         display: flex;
@@ -75,7 +118,7 @@ const onItemClick = (playlist: MyPlaylist) => {
         position: relative;
 
         input {
-            margin: 1.5rem 1.5rem 1.5rem 1.5rem;
+            margin: 0 1.5rem 1.5rem 1.5rem;
             border: 2px solid transparent;
             border-radius: 2rem;
             width: calc(100% - 3rem);
@@ -90,10 +133,18 @@ const onItemClick = (playlist: MyPlaylist) => {
             }
         }
 
-        svg {
+        button {
+            background-color: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+            padding: 0;
+            margin: 0;
             position: absolute;
             right: 2.5rem;
+            top: 1.2rem;
         }
+
     }
 
     .list-wrapper {
@@ -107,7 +158,8 @@ const onItemClick = (playlist: MyPlaylist) => {
             display: none;
         }
 
-        button {
+        >.list-item {
+            padding: 0 1.5rem 1.5rem 0;
             position: relative;
             width: fit-content;
             display: block;
@@ -135,6 +187,26 @@ const onItemClick = (playlist: MyPlaylist) => {
 
             &:hover .tooltip {
                 display: block;
+            }
+        }
+
+        .more-items-wrapper {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+
+            button {
+                background-color: #1ED760;
+                border-radius: 2rem;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                font-size: 1.8rem;
+                color: white;
+                border: none;
+                padding: 1rem;
+                margin: 1.5rem;
+                margin-top: 0;
             }
         }
     }
