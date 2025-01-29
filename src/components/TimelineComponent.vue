d00ee5470bdb3a2670f3cec98599dad6bcd83dda
trnkma <matthias.trnka@gmail.com>
2025-01-29 01:03:48 +0100

    clean up; styling changes;

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="timeline-wrapper">
+    <div>
         <div class="teams">
             <button v-for="(team) in gameLogicService.teams.value" :key="team.id"
                 @click="gameLogicService.setDisplayedTeam(team)" :class="{ active: team.display }">
@@ -8,42 +8,35 @@
             </button>
 
         </div>
+        <div class="timeline-wrapper">
+            <div class="timeline-items-wrapper">
+                <div class="timeline-items">
+                    <div v-for="(track, index) in gameLogicService.displayedTeam.value?.timeline" :key="track?.id"
+                        class="timeline-item">
+                        <div class="guess-area"
+                            @click="gameLogicService.makeGuessBefore(track as Track, gameLogicService.playingTeam.value?.timeline[index - 1] as Track)">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                                fill="#e8eaed">
+                                <path
+                                    d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                            </svg>
+                        </div>
 
-        <div class="timeline-items-wrapper">
-            <div class="timeline-items">
-                <div v-for="(track, index) in gameLogicService.displayedTeam.value?.timeline" :key="track?.id"
-                    class="timeline-item">
-                    <div class="guess-area"
-                        @click="gameLogicService.makeGuessBefore(track as Track, gameLogicService.playingTeam.value?.timeline[index - 1] as Track)">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                            fill="#e8eaed">
-                            <path
-                                d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-                        </svg>
-                    </div>
-                    <!-- <div class="guess-field" :class="{ disabled: !gameLogicService.displayedTeam.value?.isActive }">
-                    <div class="before"
-                        @click="gameLogicService.makeGuessBefore(track as Track, gameLogicService.playingTeam.value?.timeline[index - 1] as Track)">
-                    </div>
-                    <div class="after"
-                        @click="gameLogicService.makeGuessAfter(track as Track, gameLogicService.playingTeam.value?.timeline[index + 1] as Track)">
-                    </div>
-                </div> -->
-                    <AlbumComponent class="album-comp" :album="track?.album" :songName="track?.name"
-                        :artist="track?.artists[0].name"></AlbumComponent>
-                    <div v-if="index + 1 === gameLogicService.displayedTeam.value?.timeline.length" class="guess-area"
-                        @click="gameLogicService.makeGuessAfter(track as Track, gameLogicService.playingTeam.value?.timeline[index + 1] as Track)">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
-                            fill="#e8eaed">
-                            <path
-                                d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-                        </svg>
+                        <AlbumComponent class="album-comp" :album="track?.album" :songName="track?.name"
+                            :artist="track?.artists[0].name"></AlbumComponent>
+                        <div v-if="index + 1 === gameLogicService.displayedTeam.value?.timeline.length"
+                            class="guess-area"
+                            @click="gameLogicService.makeGuessAfter(track as Track, gameLogicService.playingTeam.value?.timeline[index + 1] as Track)">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                                fill="#e8eaed">
+                                <path
+                                    d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+                            </svg>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
-
     </div>
 </template>
 
@@ -61,41 +54,41 @@ const gameLogicService = GameLogicService.getInstance();
 </script>
 
 <style lang="scss" scoped>
-.timeline-wrapper {
-    overflow-y: auto;
-    width: 100%;
+.teams {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
 
-    .teams {
+    button {
+        background-color: transparent;
+        border: 1px solid white;
+        border-radius: 1rem;
         display: flex;
         align-items: center;
-        margin-bottom: 1rem;
+        justify-content: center;
+        margin-right: 2rem;
+        padding: 1rem;
 
-        button {
-            background-color: transparent;
-            border: 1px solid white;
-            border-radius: 1rem;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-right: 2rem;
-            padding: 1rem;
+        h2 {
+            color: white;
+            padding: 0;
+            margin: 0;
+
+        }
+
+        &.active {
+            background-color: white;
 
             h2 {
-                color: white;
-                padding: 0;
-                margin: 0;
-
-            }
-
-            &.active {
-                background-color: white;
-
-                h2 {
-                    color: black;
-                }
+                color: black;
             }
         }
     }
+}
+
+.timeline-wrapper {
+    overflow-y: auto;
+    width: 100%;
 
     .timeline-items-wrapper {
         width: 100%;
@@ -119,25 +112,29 @@ const gameLogicService = GameLogicService.getInstance();
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                    min-width: 1rem;
+                    min-width: 5rem;
                     height: 100%;
                     transition: min-width 0.3s;
 
-                    svg {
-                        display: none;
-                    }
+                    @media(min-width: 1024px) {
+                        min-width: 1rem;
 
-                    &:hover {
-                        min-width: 8rem;
-                        background-color: rgba(77, 77, 77, 0.466);
-                        cursor: pointer;
+                        &:hover {
+                            min-width: 8rem;
+                            background-color: rgba(77, 77, 77, 0.466);
+                            cursor: pointer;
+
+                            svg {
+                                position: absolute;
+                                top: 50%;
+                                left: 50%;
+                                transform: translate(-50%, -50%);
+                                display: block;
+                            }
+                        }
 
                         svg {
-                            position: absolute;
-                            top: 50%;
-                            left: 50%;
-                            transform: translate(-50%, -50%);
-                            display: block;
+                            display: none;
                         }
                     }
 
@@ -178,23 +175,18 @@ const gameLogicService = GameLogicService.getInstance();
 
 ::-webkit-scrollbar {
     height: 8px;
-    /* Set the scrollbar height */
 }
 
 ::-webkit-scrollbar-track {
     background: transparent;
-    /* Background of the scrollbar track */
 }
 
 ::-webkit-scrollbar-thumb {
     background: gray;
-    /* Color of the scrollbar handle */
     border-radius: 10px;
-    /* Optional: make the scrollbar round */
 }
 
 ::-webkit-scrollbar-thumb:hover {
     background: darkgray;
-    /* Darker gray on hover */
 }
 </style>
