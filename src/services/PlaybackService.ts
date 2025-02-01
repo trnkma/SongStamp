594530ca9e8ee20a4b3256143eae4edd67856542
trnkma <matthias.trnka@gmail.com>
2025-02-01 20:11:19 +0100

    adapted to class authservice removed _state object

--- src/services/PlaybackService.ts
+++ src/services/PlaybackService.ts
@@ -1,34 +1,28 @@
-import axios, { type AxiosInstance } from "axios";
-import { ref, reactive, computed } from "vue";
+import { ref, watch } from "vue";
 import { AuthService } from "./AuthService";
-import type { SpotifyPlayer, WebPlaybackPlayer, WebPlaybackState, WebPlaybackTrack } from "@/types/SpotifyWebSDK";
+import type { SpotifyPlayer, WebPlaybackPlayer, WebPlaybackState } from "@/types/SpotifyWebSDK";
 import type { Track } from "@/types/SpotifyWebAPI";
+import axios from "axios";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare global { interface Window { onSpotifyWebPlaybackSDKReady: () => void; Spotify: any; } } //no idea how this object actually looks like
 
 export class PlaybackService {
-    private apiClient: AxiosInstance;
-    private authService = AuthService();
+    private apiClient;
+    private authService = AuthService.getInstance();
     private static instance: PlaybackService;
 
-    private _state = reactive({
-        player: null as SpotifyPlayer | null,
-        deviceID: null as string | null,
-        isLoadingPlayer: ref(true),
-        isPlaying: ref(false),
-        activeTrack: ref(null as WebPlaybackTrack | null),
-        position: ref(undefined as number | undefined),
-        playerState: ref(null),
-        repeatSet: false,
-    });
+    private player: SpotifyPlayer | null = null;
+    private deviceID: string | null = null;
+    isPlaying = ref(false);
+    private repeatSet = false;
 
     private constructor() {
         this.apiClient = axios.create({
             baseURL: "https://api.spotify.com/v1",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${localStorage.getItem("spotify_auth_token")}`,
+                Authorization: `Bearer ${this.authService.access_token.value}`,
             },
         });
         this.init();
@@ -41,39 +35,28 @@ export class PlaybackService {
         return PlaybackService.instance;
     }
 
-    public get isLoadingPlayer() {
-        return computed(() => this._state.isLoadingPlayer);
-    }
-
-    public get isPlaying() {
-        return computed(() => this._state.isPlaying);
-    }
-
-    // public get hasTrack() {
-    //     return computed(() => !!this.trackService.activeTrack);
-    // }
-
-    public get state() {
-        return this._state;
-    }
 
     public togglePlay(): void {
-        this._state.player?.togglePlay();
+        this.player?.togglePlay();
     }
 
     public playTrack(track: Track | null): void {
+        console.log(this.deviceID)
         if (!track) return;
         console.log(track);
-        this.apiClient.put("/me/player/play", { uris: [track.uri] }).then(() => {
-            // this.apiClient.put('/me/player/repeat?state=track');
-        });
+        this.apiClient?.put("/me/player/play", { uris: [track.uri] }).then(() => { });
     }
 
-    private init(): void {
+    init() {
         const existingScript = document.querySelector('script[src="https://sdk.scdn.co/spotify-player.js"]');
+        watch(this.authService.access_token, (newToken) => {
+            console.log("sensed change in the token ~");
+            this.apiClient.defaults.headers.Authorization = `Bearer ${newToken}`;
+            this.createPlayer(newToken);
+        });
         if (existingScript) {
             console.warn("Spotify script is already loaded.");
-            this.createPlayer(localStorage.getItem("spotify_auth_token")!);
+            this.createPlayer(this.authService.access_token.value);
             return;
         }
         const spotifyScript = document.createElement("script");
@@ -86,87 +69,73 @@ export class PlaybackService {
         };
 
         window.onSpotifyWebPlaybackSDKReady = () => {
-            const token = localStorage.getItem("spotify_auth_token");
-            if (token) {
-                this.createPlayer(token)
-            };
+            this.createPlayer(this.authService.access_token.value);
         };
     }
 
     private createPlayer(token: string): void {
-        this._state.player = new window.Spotify.Player({
+        this.player = new window.Spotify.Player({
             name: "Vue Spotify Player",
             getOAuthToken: (cb: (token: string) => void) => cb(token),
             volume: 0.5,
         });
+        if (!this.player) return;
 
-        if (!this._state.player) return;
-
-        this._state.player.addListener("initialization_error", ({ message }: { message: string }) =>
+        this.player.addListener("initialization_error", ({ message }: { message: string }) =>
             console.error("Initialization Error:", message)
         );
-        this._state.player.addListener("authentication_error", ({ message }: { message: string }) =>
+        this.player.addListener("authentication_error", ({ message }: { message: string }) =>
             console.error("Authentication Error:", message)
         );
-        this._state.player.addListener("account_error", ({ message }: { message: string }) =>
+        this.player.addListener("account_error", ({ message }: { message: string }) =>
             console.error("Account Error:", message)
         );
-        this._state.player.addListener("playback_error", ({ message }: { message: string }) =>
+        this.player.addListener("playback_error", ({ message }: { message: string }) =>
             console.error("Playback Error:", message)
         );
 
-        this._state.player.addListener("player_state_changed", (stateData: WebPlaybackState | null) => {
-            this.setPlayingStatus(!stateData?.paused);
-            this._state.position = stateData?.position;
-            if (!this._state.repeatSet && !stateData?.paused && !stateData?.loading) {
-                this.apiClient.put("/me/player/repeat?state=track").then(() => {
-                    this._state.repeatSet = true;
+        this.player.addListener("player_state_changed", (stateData: WebPlaybackState | null) => {
+            this.isPlaying.value = !stateData?.paused;
+
+            console.log(stateData)
+            if (!this.repeatSet && !stateData?.paused && !stateData?.loading) {
+                this.apiClient?.put("/me/player/repeat?state=track").then(() => {
+                    this.repeatSet = true;
                 });
             }
             if (stateData?.paused && stateData?.position === 0 && stateData?.track_window.next_tracks.length === 0) {
-                this._state.activeTrack = null;
-                console.log(this._state.activeTrack);
+
             }
         });
 
-        this._state.player.addListener("ready", (result: WebPlaybackPlayer) => {
-            this._state.deviceID = result.device_id;
-            console.log("Ready with Device ID:", result.device_id);
+        this.player.addListener("ready", (result: WebPlaybackPlayer) => {
+            this.deviceID = result.device_id;
+            console.log("Ready with Device ID:", this.deviceID);
             this.activateDevice(result.device_id);
         });
 
-        this._state.player.addListener("not_ready", (result: WebPlaybackPlayer) => {
+        this.player.addListener("not_ready", (result: WebPlaybackPlayer) => {
             console.warn("Device went offline:", result.device_id);
         });
 
-        this._state.player.connect();
+        this.player.connect();
     }
 
     private async reconnect(): Promise<void> {
-
-        const tokenResponse = await this.authService.requestNewToken();
-        localStorage.setItem("spotify_auth_token", tokenResponse.access_token);
-        localStorage.setItem("refresh_token", tokenResponse.refresh_token);
-
-        this.activateDevice(this._state.deviceID!);
+        await this.authService.getRefreshToken();
     }
 
     private async activateDevice(deviceId: string): Promise<void> {
         try {
-            await this.apiClient.put("/me/player", { device_ids: [deviceId] });
+            await this.apiClient?.put("/me/player", { device_ids: [deviceId] });
             console.info("Device activated");
-            this._state.isLoadingPlayer = false;
         } catch (error) {
-            if (axios.isAxiosError(error) && error.response?.status === 401) {
+            if (axios.isAxiosError(error) && error.response?.status == 401) {
                 console.warn("Device not found. Reconnecting...");
                 this.reconnect();
             }
             console.error("Error activating device:", error);
+            this.reconnect();
         }
     }
-
-    private setPlayingStatus(status: boolean): void {
-        this._state.isPlaying = status;
-    }
-
 }
